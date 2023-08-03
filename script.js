4ad98ddb322f27f88ece11e7744508878a0a81f8
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-03 00:26:09 -0400

    create and style the span for the guessing word

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ const words = {
     animal: ["cat", "dog","lion", "horse", "duck", "kangroo"],
     fruit :["apple", "banana" ,"orange", "cherry", "watermelon", "peach"],
     vegetable: ['carrot', 'broccoli','spinach', 'onion', 'potato', 'tomato'],
-    place:['paris',"newyork",'tokyo', 'jerusalem', 'rome', 'egypt'],
+    place:['paris',"new york",'tokyo', 'jerusalem', 'rome', 'egypt'],
     color:['red' ,'blue','green', 'pink', 'purple'],
 }
 
 //get random property
 let allKeys = Object.keys(words)
-console.log(allKeys)
+
 //choose random index depends on keys length
 let randomPropNumber = Math.floor(Math.random()* allKeys.length);
-console.log(randomPropNumber)
+
 //get the name of the key index --category
 let randomPropName = allKeys[randomPropNumber]
-console.log(randomPropName)
+
 //get random value --category words
 let randomPropValue = words[randomPropName]
 console.log(randomPropValue)
@@ -65,4 +65,16 @@ let lettersGuessContainer = document.querySelector(".letters-guess");
 
 //convert chosen word to array
 let lettersAndSpace = Array.from(randomValueValue);
-console.log(lettersAndSpace)
+
+//create spans depend on word
+lettersAndSpace.forEach(letter =>{
+    //create empty span
+    let emptySpan = document.createElement("span");
+    //if letter is space
+    if(letter === " "){
+        //add class to the span
+        emptySpan.className = 'with-space';
+    }
+    //append span to letters guess container
+    lettersGuessContainer.appendChild(emptySpan);
+})
