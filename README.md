d0409d6414302dbf184408ef5dff4724c3e640c3
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-22 11:35:34 -0400

    add readme file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Hangman-Game
+
+## Description
+The Hangman Game is a classic word-guessing game where players attempt to guess a hidden word one letter at a time. This project is a simple implementation of the game using HTML, CSS, and JavaScript.
+
+![](./assets/hangman.PNG)
+![](./assets/Capture.PNG)
+![](./assets/hang3.PNG)
+
+## Features
+Random Word Selection: The game randomly selects a word from predefined categories like animals, fruits, and more.
+
+User Interaction: Players can click on letter boxes to make guesses.
+
+Feedback: The game provides feedback on correct and incorrect guesses.
+
+Hangman Drawing: A hangman drawing appears as incorrect guesses accumulate.
+
+Winning and Losing: The game checks if the player successfully guessed the word or if they've made too many incorrect guesses.
+
+Pop-up Messages: Informative pop-up messages display at the end of the game to notify the player of the outcome.
+
+## How to Play
+Clone this repository to your local machine.
+
+Open the index.html file in your web browser.
+
+Click on the letter boxes to make guesses.
+
+Try to guess the word before you run out of incorrect attempts!
+
--- script.js
+++ script.js
@@ -2,62 +2,59 @@
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
 //get array from letters
-let lettersArray= Array.from(letters);
+let lettersArray = Array.from(letters);
 
 //select letters container
 let lettersContainer = document.querySelector(".letters");
 
 //generate letters
-lettersArray.forEach(letter =>{
+lettersArray.forEach((letter) => {
+  //create span
+  let span = document.createElement("span");
 
-    //create span
-    let span =document.createElement("span")
+  //create letter text node
+  let theLetter = document.createTextNode(letter);
 
-    //create letter text node
-    let theLetter  = document.createTextNode(letter)
+  //append the letter to span
+  span.appendChild(theLetter);
 
-    //append the letter to span
-    span.appendChild(theLetter);
+  //add class on span
+  span.className = "letter-box";
 
-    //add class on span
-    span.className = 'letter-box';
-    
-    //append span to the letters container
-    lettersContainer.appendChild(span);
-
+  //append span to the letters container
+  lettersContainer.appendChild(span);
 });
 
 //object of words + categories
 const words = {
-    animal: ["cat", "dog","lion", "horse", "duck", "kangroo"],
-    fruit :["apple", "banana" ,"orange", "cherry", "watermelon", "peach"],
-    vegetable: ['carrot', 'broccoli','spinach', 'onion', 'potato', 'tomato'],
-    place:['paris',"new york",'tokyo', 'jerusalem', 'rome', 'egypt'],
-    color:['red' ,'blue','green', 'pink', 'purple'],
-}
+  animal: ["cat", "dog", "lion", "horse", "duck", "kangroo"],
+  fruit: ["apple", "banana", "orange", "cherry", "watermelon", "peach"],
+  vegetable: ["carrot", "broccoli", "spinach", "onion", "potato", "tomato"],
+  place: ["paris", "new york", "tokyo", "jerusalem", "rome", "egypt"],
+  color: ["red", "blue", "green", "pink", "purple"],
+};
 
 //get random property
-let allKeys = Object.keys(words)
+let allKeys = Object.keys(words);
 
 //choose random index depends on keys length
-let randomPropNumber = Math.floor(Math.random()* allKeys.length);
+let randomPropNumber = Math.floor(Math.random() * allKeys.length);
 
 //get the name of the key index --category
-let randomPropName = allKeys[randomPropNumber]
+let randomPropName = allKeys[randomPropNumber];
 
 //get random value --category words
-let randomPropValue = words[randomPropName]
-console.log(randomPropValue)
+let randomPropValue = words[randomPropName];
+console.log(randomPropValue);
 //choose a word randomly in that category and get its position number (index) within this list using
-let randomValueNumber = Math.floor(Math.random()* randomPropValue.length)
-console.log(randomValueNumber)
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
+console.log(randomValueNumber);
 //the chosen word
 let randomValueValue = randomPropValue[randomValueNumber];
 console.log(randomValueValue);
 
 //set category info
 document.querySelector(".game-info .category span").innerHTML = randomPropName;
-
 
 //generate guess elements
 //select letters guess element
@@ -67,17 +64,17 @@ let lettersGuessContainer = document.querySelector(".letters-guess");
 let lettersAndSpace = Array.from(randomValueValue);
 
 //create spans depend on word
-lettersAndSpace.forEach(letter =>{
-    //create empty span
-    let emptySpan = document.createElement("span");
-    //if letter is space
-    if(letter === " "){
-        //add class to the span
-        emptySpan.className = 'with-space';
-    }
-    //append span to letters guess container
-    lettersGuessContainer.appendChild(emptySpan);
-})
+lettersAndSpace.forEach((letter) => {
+  //create empty span
+  let emptySpan = document.createElement("span");
+  //if letter is space
+  if (letter === " ") {
+    //add class to the span
+    emptySpan.className = "with-space";
+  }
+  //append span to letters guess container
+  lettersGuessContainer.appendChild(emptySpan);
+});
 
 //select guess spans
 let guessSpan = document.querySelectorAll(".letters-guess span");
@@ -86,10 +83,10 @@ let guessSpan = document.querySelectorAll(".letters-guess span");
 let wrongAttempts = 0;
 
 //select the draw element
-let theDraw = document.querySelector(".hangman-draw")
+let theDraw = document.querySelector(".hangman-draw");
 
 //handle clicking on letters
-document.addEventListener("click", (e) =>{
+document.addEventListener("click", (e) => {
   //set the chosen the if true or not
   let theStatus = false;
   if (e.target.className === "letter-box") {
@@ -115,39 +112,79 @@ document.addEventListener("click", (e) =>{
         });
       }
     });
-   //if letter is wrong
-   if(theStatus !== true){
-    //increase the wrong attempts
-    wrongAttempts++;
+    //if letter is wrong
+    if (theStatus !== true) {
+      //increase the wrong attempts
+      wrongAttempts++;
 
-    //add wrong class to draw element
-    theDraw.classList.add("wrong-" + wrongAttempts);
+      //add wrong class to draw element
+      theDraw.classList.add("wrong-" + wrongAttempts);
 
-    //finish the game
-   
-    if (wrongAttempts ===8){
-      endGame();
-      lettersContainer.classList.add("finished");
+      //finish the game
+
+      if (wrongAttempts === 8) {
+        endGame();
+        lettersContainer.classList.add("finished");
+      }
     }
-   }
   }
-})
+});
 
 //end game function
-function endGame(){
-    //create pop-up div
-    let div= document.createElement('div');
+// function endGame() {
+//   //create pop-up div
+//   let div = document.createElement("div");
 
-    //create text
-    let divText= document.createTextNode(`Game Over, The word is ${randomValueValue}`)
-    
-    //append text to div
-    div.appendChild(divText);
+//   //create text
+//   let divText = document.createTextNode(
+//     `Game Over, The word is ${randomValueValue}`
+//   );
 
-    //add class on div
-    div.className="popup";
+//   //append text to div
+//   div.appendChild(divText);
 
-    //apend to the body
-    document.body.appendChild(div);
+//   //add class on div
+//   div.className = "popup";
+
+//   //apend to the body
+//   document.body.appendChild(div);
+// }
+
+
+let correctGuesses = 0;
+
+// Inside the click event handler
+if (wordLetter == theClickedLetter) {
+    // ...
+    correctGuesses++;
+
+    // Check if the player has guessed all the letters
+    if (correctGuesses === randomValueValue.length) {
+        endGame(true); // Call endGame with a win condition
+        lettersContainer.classList.add("finished");
+    }
 }
 
+function endGame(isWin) {
+  // Create a pop-up div
+  let div = document.createElement("div");
+
+  if (isWin) {
+    // Create a winning message
+    let divText = document.createTextNode(
+      `Congratulations, You won! The word is ${randomValueValue}`
+    );
+    div.appendChild(divText);
+    div.className = "popup-win";
+  } else {
+    // Create a losing message
+    let divText = document.createTextNode(
+      `Game Over, The word is ${randomValueValue}`
+    );
+    div.appendChild(divText);
+    div.className = "popup-lose";
+  }
+
+  // Append to the body
+  document.body.appendChild(div);
+}
